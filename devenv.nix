2e52fa6f12326f4efe7ebabdadd84bf91adc0ddc
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2023-05-11 04:14:21 +0200

    ✨ Initial commit

---
+++ devenv.nix
@@ -0,0 +1,13 @@
+{ pkgs, ... }:
+
+{
+  packages = [ pkgs.git ];
+
+  languages.python = {
+    enable = true;
+    poetry = {
+      enable = true;
+    };
+  };
+  languages.typescript.enable = true;
+}
