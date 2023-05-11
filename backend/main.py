12677c5261852d53c838a987435e1b78ba21e3b4
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2023-05-11 05:44:12 +0200

    🔒 Setup CORS for fastapi

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,23 @@
 from typing import Union, Optional
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from pydantic import BaseModel
 
+
+origins = [
+    "http://localhost:3000",
+]
+
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 class Student(BaseModel):
